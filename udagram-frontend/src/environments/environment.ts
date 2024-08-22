e06a9f4742435c3409187506610c0b6e5873a31b
Tinh Bui Duong <bdtinh1999@gmail.com>
2024-08-22 11:59:13 +0700

    init

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a68f4aa0d8f2242458e1b1996ad4ecfb-270834660.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
